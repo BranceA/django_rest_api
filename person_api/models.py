ac8188d13c38b26e61c90931b21f44aa55f8e9d5
BranceA <brancealexander@gmail.com>
2021-05-25 13:28:39 -0500

    added default to datefield so migrations can work properly

--- person_api/models.py
+++ person_api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
-    date_joined = models.DateField
-    date_updated = models.DateField
+    date_joined = models.DateField(default='1111-11-11')
+    date_updated = models.DateField(default='1111-11-11')
 
     def __str__(self):
         return self.name
