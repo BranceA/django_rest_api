63645de7e8c917a3ba1983111dd3eb2317aceac8
BranceA <brancealexander@gmail.com>
2021-06-10 13:46:05 -0500

    added jobs to setup and added job tests in the person test

--- person_api/tests.py
+++ person_api/tests.py
@@ -1,15 +1,26 @@
-from django.test import TestCase
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
 import datetime
 
 from .models import Person, Job
+from .serializers import PersonSerializer, JobSerializer
+
+client = Client()
 
 class PersonTest(TestCase):
     # Test for person model
     def setUp(self):
-        Person.objects.create(age=30, date_joined='2021-05-10', date_updated='2021-05-15', name='Bob Doe')
-        Person.objects.create(age=45, date_joined='2021-05-02', date_updated='2021-05-15', name='Sally Doe')
-        Person.objects.create(age=7, date_joined='2021-08-10', date_updated='2021-11-10', name='Billy Doe')
-        Person.objects.create(age=22, date_joined='2020-02-10', date_updated='2021-12-25', name='Bertha Doe')
+        clown = Job.objects.create(job_title='Circus Clown', salary=35000)
+        doctor = Job.objects.create(job_title='Doctor', salary=120000)
+        realtor = Job.objects.create(job_title='Realtor', salary=62000)
+        professor = Job.objects.create(job_title='Professor', salary=50000)
+
+        Person.objects.create(age=30, date_joined='2021-05-10', date_updated='2021-05-15', name='Bob Doe', job=clown)
+        Person.objects.create(age=45, date_joined='2021-05-02', date_updated='2021-05-15', name='Sally Doe', job=doctor)
+        Person.objects.create(age=7, date_joined='2021-08-10', date_updated='2021-11-10', name='Billy Doe', job=realtor)
+        Person.objects.create(age=22, date_joined='2020-02-10', date_updated='2021-12-25', name='Bertha Doe', job=professor)
 
     def test_person(self):
         bob = Person.objects.get(name='Bob Doe')
@@ -28,3 +39,17 @@ class PersonTest(TestCase):
         # test date updated has been created correctly
         self.assertEqual(bob.date_updated, datetime.date(2021, 5, 15))
         self.assertEqual(billy.date_updated, datetime.date(2021, 11, 10))
+        # test to see if job is added correctly
+        self.assertEqual(sally.job.job_title, 'Doctor')
+        self.assertEqual(sally.job.salary, 120000)
+        self.assertEqual(billy.job.job_title, 'Realtor')
+        self.assertEqual(billy.job.salary, 62000)
+
+    # def test_get_all_people(self):
+    #     # get API response
+    #     response = client.get(reverse('ShowAll'))
+    #     # get data from db
+    #     people = Person.objects.all()
+    #     serializer = PersonSerializer(people, many=True)
+    #     self.assertEqual(response.data, serializer.data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
